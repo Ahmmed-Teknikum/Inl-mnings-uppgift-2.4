0cc4f5f2895f5d42172010810b80c9124ecd3b21
ahka07001 <ahka07001@E-PW05CGQT>
2024-10-03 14:28:41 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inlämning_._uppgift_._2_._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din lön?");
+            int lön1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är din lön?");
+            int lön2= int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är din lön?");
+            int lön3= int.Parse(Console.ReadLine());
+
+            int medelvärde = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medelvärdet mellan personalen " + medelvärde);
+        }
+    }
+}
